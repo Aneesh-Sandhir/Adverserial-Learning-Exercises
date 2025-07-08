7f8c5257ed5e77c6521f1c6224772465e8fc805f
Aneesh Sandhir <52419932+Aneesh-Sandhir@users.noreply.github.com>
2025-07-08 18:50:25 -0400

    ammended chart

--- FGSM_Pytorch.py
+++ FGSM_Pytorch.py
@@ -11,7 +11,6 @@ from torchvision import transforms
 from PIL import Image
 import requests
 
-import numpy as np
 import matplotlib.pyplot as plt
 
 class FGSM_pytorch:
@@ -30,7 +29,6 @@ class FGSM_pytorch:
         self.adverserial_pattern = self.create_adverserial_pattern()
 
     def create_adverserial_pattern(self):       
-        
         label_vector = torch.zeros_like(self.original_prediction)
         category_index = self.model_categories.index(self.label)
         label_vector[category_index] = 1
@@ -51,14 +49,12 @@ class FGSM_pytorch:
         return adverserial_pattern
     
     def augment_image(self, epsilon):
-        
         augmneted_image = self.image_tensor + (epsilon * self.adverserial_pattern)
         augmneted_image = torch.clamp(augmneted_image, -1, 1)
         
         return augmneted_image
         
     def make_prediction(self, image_tensor):
-        
         self.pytorch_model.eval()
         
         with torch.no_grad():
@@ -68,7 +64,6 @@ class FGSM_pytorch:
         return prediction 
 
     def prepare_image(self, image):
-        
         preprocessing = transforms.Compose([
             transforms.Resize((224, 244)),
             transforms.ToTensor(),
@@ -80,15 +75,12 @@ class FGSM_pytorch:
         return mini_batch
         
     def get_predicted_category(self, model_output):
-        
-        highest_probability_index = model_output[0].argmax()
+        highest_probability_index = model_output.argmax()
         predicted_category = self.model_categories[highest_probability_index]
         
         return predicted_category
     
     def get_category_probability(self, category, image_predictions):
-        
-        #image_predictions = torch.nn.functional.softmax(model_output[0], dim = 0)
         category_probability = 0
         try:
             category_index = self.model_categories.index(category)
@@ -119,15 +111,56 @@ if __name__ == "__main__":
     attack.plot_image(attack.image_tensor)
     attack.plot_image(attack.adverserial_pattern)
     
-    epsilons = torch.linspace(0, 1, steps = 26)
-    confidence_vector = torch.zeros_like(epsilons)
-    for index, epsilon in enumerate(epsilons):
-        augment_image = attack.augment_image(epsilon)
-        prediction = attack.make_prediction(augment_image)
-        confidence_vector[index] = attack.get_category_probability(image_label, prediction)
-        
-    plt.plot(epsilons, confidence_vector)
-        
-        
+    epsilons = torch.linspace(0, 1, steps = 251)
+    confidence_label_vector = torch.zeros_like(epsilons)
+    confidence_prediction_vector = torch.zeros_like(epsilons)
     
+    largest_delta = 0
+    largest_delta_image = None
+    largest_delta_poi= 0
+    smallest_delta = 1
+    smallest_delta_image = None
+    smallest_delta_poi = 0
+    noisiest_properly_classified_image = image
+    noisiest_proper_classification_poi = 0
+    
+    for index, epsilon in enumerate(epsilons):
+        augmented_image = attack.augment_image(epsilon)
+        output = attack.make_prediction(augmented_image)
+        label_confidence = attack.get_category_probability(image_label, output)
+        confidence_label_vector[index] = label_confidence
         
+        predicted_class = attack.get_predicted_category(output)
+        predicted_class_confidence = attack.get_category_probability(predicted_class, output)
+        confidence_prediction_vector[index] = predicted_class_confidence
+        
+        delta = predicted_class_confidence - label_confidence
+        if (delta == 0):
+            noisiest_properly_classified_image = augmented_image
+            noisiest_proper_classification_poi = epsilon
+        
+        if (delta > largest_delta):
+            largest_delta = delta
+            largest_delta_image = augmented_image
+            largest_delta_poi = index
+            
+        if (delta < smallest_delta) & (delta > 0):
+            smallest_delta = delta
+            smallest_delta_image = augmented_image
+            smallest_delta_poi = index    
+
+    attack.plot_image(noisiest_properly_classified_image)
+    attack.plot_image(largest_delta_image)
+    attack.plot_image(smallest_delta_image)
+    
+    plt.ylabel('Confidence')
+    plt.xlabel('Epsilon Value')
+    plt.plot(epsilons, confidence_prediction_vector, label = 'Prediction Confidence')
+    plt.plot(epsilons, confidence_label_vector, label = 'Labeled Class Confidence')
+    
+    plt.vlines(epsilons[largest_delta_poi], 0, confidence_prediction_vector[largest_delta_poi], 
+               label = 'Largest Confidence Gap', color = 'limegreen')
+    plt.vlines(epsilons[smallest_delta_poi], 0, confidence_prediction_vector[smallest_delta_poi],
+               label = 'Smallest Confidence Gap', color = 'red')
+    
+    plt.legend()
